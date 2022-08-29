ab7a82449d54db890c30746289b869cec4d0943a
robs-am <roberta.amaro89@gmail.com>
2022-08-29 16:09:44 -0300

    add funcao onclick

--- js/main.js
+++ js/main.js
@@ -1,13 +1,22 @@
 function tocaSomPom () {
-    document.querySelector('.tecla_pom');
-    //seleiona a tecla pom
+  
 
     document.querySelector('#som_tecla_pom').play()
+  
  
     //executa a função play da tecla pom
 }
+
+document.querySelector('.tecla_pom').onclick = tocaSomPom;
+
+//onclick é um atributo e com o = definimos um valor a ele usando a classe do button correspondente a tecla. quando o onclick é executado é executado a funçao tocaSomPom
+
+//nao colocamos o parenteses ao final da funcao tocaSomPom pois ja extamos a executando na função onclick
+
+
 
 function tocaSomClap () {
     document.querySelector ('.tecla_clap');
     document.querySelector('#som_tecla_clap').play()
 }
+
