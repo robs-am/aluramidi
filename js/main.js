837a55e5b66fb88db6649434f8907670570c3c4a
robs-am <roberta.amaro89@gmail.com>
2022-08-29 15:07:00 -0300

    add function tocaSomPom

--- js/main.js
+++ js/main.js
@@ -1,1 +1,9 @@
-document.querySelector('.tecla_pom');
+function tocaSomPom () {
+    document.querySelector('.tecla_pom');
+    //seleiona a tecla pom
+
+    document.querySelector('#som_tecla_pom').play()
+ 
+    //executa a função play da tecla pom
+}
+
